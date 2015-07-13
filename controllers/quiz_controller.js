6ad39b1f9934f5b966b88d93f87e9b10d0f97d6e
Aaron Torrijos <aaron.torri@gmail.com>
2015-07-13 19:38:15 +0200

    Cambio en busqueda en BD, findById por find

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,7 +3,7 @@ var models = require('../models/models.js')
 //GET /quizes
 
 exports.index = function(req, res){
-	models.Quiz.findAll().then(function(quiz){
+	models.Quiz.findAll().then(function(quizes){
 		res.render('quizes/index.ejs',{quizes: quizes});
 	});
 };
@@ -11,7 +11,7 @@ exports.index = function(req, res){
 //GET /quizes/:id
 
 exports.show = function(req, res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
 		res.render('quizes/show',{quiz: quiz});
 	});
 };
@@ -19,7 +19,7 @@ exports.show = function(req, res){
 //GET /quizes/:id/answer
 
 exports.answer = function(req, res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
 		if(req.query.respuesta === quiz.respuesta){
 			res.render('quizes/answer',{quiz: quiz, respuesta: 'Correcto'});
 		} else {
