f51a282bb27acb80d175f9d46ea665f2e850a8e4
Aaron Torrijos <aaron.torri@gmail.com>
2015-07-14 19:59:07 +0200

    Añadido buscador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,9 +19,17 @@ exports.load = function(req, res, next, quizId){
 //GET /quizes
 
 exports.index = function(req, res){
-	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index',{quizes: quizes});
-	}).catch(function(error){next(error);});
+	if (!!req.query && !!req.query.search){
+		//console.log("parametro: " + req.query.search);
+		var search = "%" + req.query.search.replace(/ /g, "%") + "%";
+		models.Quiz.findAll({where: ["pregunta like ?", search]}).then(function(quizes){
+			res.render('quizes/index',{quizes: quizes});
+		}).catch(function(error){next(error);});
+	} else {
+		models.Quiz.findAll().then(function(quizes){
+			res.render('quizes/index',{quizes: quizes});
+		}).catch(function(error){next(error);});
+	}
 };
 
 //GET /quizes/:id
